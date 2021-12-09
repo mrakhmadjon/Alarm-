5cc8821e147e6d28ded2aa45cc64ad19f340c8c0
Akhmadjon <72850225+mrakhmadjon@users.noreply.github.com>
2021-12-09 14:44:35 +0500

    Add files via upload
    A simple Alarm clock

---
+++ Simple Alarm Clock/Simple Alarm Clock/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace MyFirstLesson
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string msg = "Tuuur o'rningdaaan";
+            int scheduleHour = DateTime.Now.Hour, scheduleMin = DateTime.Now.Minute;
+
+            Thread budilnik = new Thread(() =>
+            {
+                while (true)
+                {
+                    int hour = DateTime.Now.Hour;
+                    int min = DateTime.Now.Minute;
+                   
+                    if (hour == scheduleHour && min == scheduleMin)
+                    {
+                        ProcessStartInfo info = new ProcessStartInfo();
+                        info.FileName = @"d:\nasheed.mp3";
+                        info.UseShellExecute = true;
+                        Process.Start(info);
+                        Console.WriteLine(msg);
+                        break;
+                    }
+                }
+            });
+
+            budilnik.Start();
+            
+        
+        }
+    }
+}
